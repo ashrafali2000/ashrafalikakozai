b9987fb7089fa820992a33490e6e0ce7094be1c9
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-16 16:46:02 +0500

    add browserroute in index file

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import './Home.css';
 function Home() {
 
   return (
     <div className="Home ">
+      <Routes>
+        <Route >
        <Layout></Layout>
-       
+
+        </Route>
+
+      </Routes>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './Home';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <BrowserRouter>
 <Home />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
