52f366ecc296abcd38d742145a7501a5e18ed3a9
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-16 16:58:28 +0500

    add the layout route in app js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
-import './App.css';
+import "./App.css";
 import Home from "./pages/home/Home";
 function App() {
-
   return (
     <div className="App">
       <Routes>
-        <Route path="/"  element={<Layout></Layout>} />
+        <Route path="/" element={<Layout></Layout>}>
           <Route index element={<Home></Home>} />
-
+        </Route>
       </Routes>
-
     </div>
   );
 }
