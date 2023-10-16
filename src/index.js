b86fbea62cb7d80cee7ef702ddd0fdf8f2fe26ed
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-16 16:53:14 +0500

    update home component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Home';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './Home';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-<Home />
+<App />
     </BrowserRouter>
   </React.StrictMode>
 );
