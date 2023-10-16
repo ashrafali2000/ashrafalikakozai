78c28f8b21873447d1908057510725ec36551117
Ashraf-ali-2000 <122337346+Ashraf-ali-2000@users.noreply.github.com>
2023-10-16 14:45:15 +0500

    install tailwind css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Home';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<Home />
   </React.StrictMode>
 );
 
