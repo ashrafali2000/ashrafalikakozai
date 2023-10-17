d630b400dd1772cdb54dc3bb0b68c49cf2d3b673
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-17 16:43:39 +0500

    create header section in home comp

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,47 @@
-import React from 'react'
-
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
+    <div className="relative overflow-hidden bg-slate-800 text-gray-600">
+      <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+          <div className="sm:max-w-lg">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-400 sm:text-6xl">
+              I'm Ashraf ali
+            </h1>
+            <p className="mt-4 text-xl text-gray-500">
+              The best Developer work with the team to improve the idea for more technics
+            </p>
+          </div>
+          <div>
+            <div className="mt-10">
+              {/* Decorative image grid */}
+              <div
+                aria-hidden="true"
+                className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
+              >
+                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                  <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <img
+                      src = "https://img.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg"
+                      alt=""
+                       className="h-full w-full object-cover object-center"
+                       />
+                      </div>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <a
+                href="#"
+                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+              >
+                Shop Collection
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default Home
